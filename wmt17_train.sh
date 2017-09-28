b55058a6e5adcc7a33580127a9afb3367f74a2e8
twairball <twairball@yahoo.com>
2017-09-28 11:09:52 +0800

    decreased fconv batchsize to accomodate 8GB GPU

--- wmt17_train.sh
+++ wmt17_train.sh
@@ -12,7 +12,7 @@ fairseq preprocess -sourcelang en -targetlang zh \
 # Fully convolutional sequence-to-sequence model
 mkdir -p $TRAIN/fconv
 fairseq train -sourcelang en -targetlang zh -datadir $DATADIR \
-    -model fconv -nenclayer 4 -nlayer 3 -dropout 0.2 -optim nag -lr 0.25 -clip 0.1 \
+    -model fconv -nenclayer 4 -nlayer 3 -batchsize 16 -dropout 0.2 -optim nag -lr 0.25 -clip 0.1 \
     -momentum 0.99 -timeavg -bptt 0 -savedir $TRAIN/fconv
 
 # Standard bi-directional LSTM model
