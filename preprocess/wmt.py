2d246c997e007502ccf36dcab7280815e7b2805f
twairball <twairball@yahoo.com>
2017-10-07 23:41:32 +0800

    added clean

--- preprocess/wmt.py
+++ preprocess/wmt.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+
 """
 Download and prepare dataset for wmt17 zh-en
 """
-import prepare
+from __future__ import unicode_literals, division
 
+import sys
+import codecs
+import io
+import argparse
+
+import prepare
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -36,6 +44,16 @@ DATA_DIR  = "data/wmt17_en_zh/"
 TMP_DIR = "tmp/wmt17_en_zh/"
 
 if __name__ == '__main__':
+    # python 2/3 compatibility
+    if sys.version_info < (3, 0):
+        sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
+        sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+        sys.stdin = codecs.getreader('UTF-8')(sys.stdin)
+    else:
+        sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+        sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+        sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', write_through=True, line_buffering=True)
+
     for ds in [WMT17_TRAIN_ZHEN, WMT17_DEV_ZHEN, WMT17_TEST_ZHEN]:
         prepare.prepare_dataset(DATA_DIR, TMP_DIR, ds)
 
--- wmt17_prepare.sh
+++ wmt17_prepare.sh
@@ -5,7 +5,13 @@ DATADIR=data-bin/wmt17_en_zh
 TRAIN=trainings/wmt17_en_zh
 
 # download, unzip, clean and tokenize dataset. 
-python preprocess/wmt.py
+python ./preprocess/wmt.py
+
+# clean empty and long sentences, and sentences with high source-target ratio (training corpus only)
+MOSESDECODER=../mosesdecoder
+$MOSESDECODER/scripts/training/clean-corpus-n.perl $TEXT/train en zh $TEXT/train.clean 1 80
+$MOSESDECODER/scripts/training/clean-corpus-n.perl $TEXT/valid en zh $TEXT/valid.clean 1 80
+$MOSESDECODER/scripts/training/clean-corpus-n.perl $TEXT/test en zh $TEXT/test.clean 1 80
 
 # build subword vocab
 SUBWORD_NMT=../subword-nmt
@@ -14,12 +20,12 @@ NUM_OPS=32000
 # learn codes and encode separately
 CODES=codes.${NUM_OPS}.bpe
 echo "Encoding subword with BPE using ops=${NUM_OPS}"
-$SUBWORD_NMT/learn_bpe.py -s ${NUM_OPS} < $TEXT/train.en > $TEXT/${CODES}.en
-$SUBWORD_NMT/learn_bpe.py -s ${NUM_OPS} < $TEXT/train.zh > $TEXT/${CODES}.zh
+$SUBWORD_NMT/learn_bpe.py -s ${NUM_OPS} < $TEXT/train.clean.en > $TEXT/${CODES}.en
+$SUBWORD_NMT/learn_bpe.py -s ${NUM_OPS} < $TEXT/train.clean.zh > $TEXT/${CODES}.zh
 
 echo "Applying vocab to training"
-$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en < $TEXT/train.en > $TEXT/train.${NUM_OPS}.bpe.en
-$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.zh < $TEXT/train.zh > $TEXT/train.${NUM_OPS}.bpe.zh
+$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en < $TEXT/train.clean.en > $TEXT/train.${NUM_OPS}.bpe.en
+$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.zh < $TEXT/train.clean.zh > $TEXT/train.${NUM_OPS}.bpe.zh
 
 VOCAB=vocab.${NUM_OPS}.bpe
 echo "Generating vocab: ${VOCAB}.en"
@@ -30,13 +36,13 @@ cat $TEXT/train.${NUM_OPS}.bpe.zh | $SUBWORD_NMT/get_vocab.py > $TEXT/${VOCAB}.z
 
 # encode validation
 echo "Applying vocab to valid"
-$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en --vocabulary $TEXT/${VOCAB}.en < $TEXT/valid.en > $TEXT/valid.${NUM_OPS}.bpe.en
-$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.zh --vocabulary $TEXT/${VOCAB}.zh < $TEXT/valid.zh > $TEXT/valid.${NUM_OPS}.bpe.zh
+$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en --vocabulary $TEXT/${VOCAB}.en < $TEXT/valid.clean.en > $TEXT/valid.${NUM_OPS}.bpe.en
+$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.zh --vocabulary $TEXT/${VOCAB}.zh < $TEXT/valid.clean.zh > $TEXT/valid.${NUM_OPS}.bpe.zh
 
 # encode test
 echo "Applying vocab to test"
-$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en --vocabulary $TEXT/${VOCAB}.en < $TEXT/test.en > $TEXT/test.${NUM_OPS}.bpe.en
-$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en --vocabulary $TEXT/${VOCAB}.zh < $TEXT/test.zh > $TEXT/test.${NUM_OPS}.bpe.zh
+$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en --vocabulary $TEXT/${VOCAB}.en < $TEXT/test.clean.en > $TEXT/test.${NUM_OPS}.bpe.en
+$SUBWORD_NMT/apply_bpe.py -c $TEXT/${CODES}.en --vocabulary $TEXT/${VOCAB}.zh < $TEXT/test.clean.zh > $TEXT/test.${NUM_OPS}.bpe.zh
 
 # generate preprocessed data
 echo "Preprocessing datasets..."
